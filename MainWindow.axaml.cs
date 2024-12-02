04d48622895845412d76386f64bbc4491e0f01b1
Numix <61939112+NumixAvali@users.noreply.github.com>
2024-12-02 16:47:59 +0200

    Yes

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace PeterAlert;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+}
