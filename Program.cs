2e8f3f9d68f9d6a6bc921d3c5b56e35fd2c072fa
Peter Strick <anitastriclk@gmail.com>
2025-04-10 11:31:05 +0200

    supporting now multiple goobers

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ class Program
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
+            .UsePlatformDetect();
+    //.WithInterFont()
+    //.LogToTrace()
 }
---
+++ build.ps1
@@ -0,0 +1,15 @@
+﻿Get-ChildItem ".\Assets" | ForEach-Object {
+    $Filename = $_.BaseName
+
+    (Get-Content MainWindow.axaml) | ForEach-Object {
+        $_ -replace "Peter", $Filename
+    } | Set-Content MainWindow.axaml
+
+    (Get-Content PeterAlert.csproj) | ForEach-Object {
+        $_ -replace "Peter", $Filename
+    } | Set-Content PeterAlert.csproj
+
+    dotnet publish -c Release --sc true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:IncludeAllContentForSelfExtract=true -o "./build/$Filename"
+
+    git reset
+}
