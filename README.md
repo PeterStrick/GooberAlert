e5f4698ecfd363e3afd9494482fd2ed34de18ef4
Peter Strick <anitastriclk@gmail.com>
2025-04-10 13:45:22 +0200

    Multi Goober Alerts ready for action

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-﻿# Yes
-![Peter](Assets/Peter.png)
+﻿# Goober Alert
+
+A fork of [PeterAlert](https://github.com/NumixAvali/PeterAlert) which allows multiple Goober Alerts to be created from their respective PNG File in the Assets Folder.
+
+# Usage
+
+1. Clone the Github Repo to somewhere
+2. Add/Replace PNGs in the Assets Folder
+3. Run the `build.ps1` PowerShell Script
+
+## Note
+
+The produced executables Name depends on the Name of the PNG File in the Assets Folder, eg having a `Lu The Shiny Thing.png` File in the Assets Folder, will produce a "Lu The Shiny Thing Alert"
+
+## Building
+
+- Git and .Net 7 are required to Build the project
+- The use of the build.ps1 Script is to automatically replace each instance of the Word `Peter` with the File name of the PNG File, this can be rewritten in another Language eg Bash or done manually as the Word `Peter` only appears inside of `MainWindow.axaml` and `GooberAlert.csproj`
+  - After those words were replaced and dotnet published the executable, a `git reset --hard` is done to revert the renaming and move onto the next PNG File
--- build.ps1
+++ build.ps1
@@ -1,4 +1,42 @@
-﻿Get-ChildItem ".\Assets" | ForEach-Object {
+﻿# Get Platform to build the Goobers for
+function Show-Menu {
+    Clear
+    Write-Host "Please enter the respective Number of a Platform to build for"
+    Write-Host ""
+    Write-Host "(1) Windows x64"
+    Write-Host "(2) Linux x64"
+    Write-Host "(3) Mac x64"
+    Write-Host ""
+    Write-Host "(4) Windows Arm (64-bit)"
+    Write-Host "(5) Linux Arm (64-bit)"
+    Write-Host "(6) Mac Arm (64-bit)"
+    Write-Host ""
+    $selection = Read-Host "Selected Platform"
+
+    switch ($selection)
+    {
+        '1' {
+            return "win-x64"
+        } '2' {
+            return "linux-x64"
+        } '3' {
+            return "osx-x64"
+        } '4' {
+            return "win-arm64"
+        } '5' {
+            return "linux-arm64"
+        } '6' {
+            return "osx-arm64"
+        } default {
+            Show-Menu
+        }
+    }
+}
+
+$Platform = Show-Menu
+
+# Build a goober alert for each PNG File
+Get-ChildItem ".\Assets" | ForEach-Object {
     $Filename = $_.BaseName
     echo $Filename
 
@@ -10,7 +48,7 @@
         $_ -replace "Peter", $Filename
     } | Set-Content GooberAlert.csproj -Encoding UTF8
 
-    dotnet publish GooberAlert.csproj -c Release --sc true /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:IncludeAllContentForSelfExtract=true -o "./build/$Filename"
+    dotnet publish GooberAlert.csproj -c Release -r $Platform --sc true -o "./build/$Filename"
 
     git reset --hard
 }
