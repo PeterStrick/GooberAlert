0cb9aad63b81fd97021b4595e6a0e196227e639c
Peter Strick <anitastriclk@gmail.com>
2025-04-14 19:28:41 +0200

    Properly exit Fix 1/?
    Co-authored-by: Wroclaw <wroclaw223@outlook.com>

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Threading;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -6,33 +7,50 @@ using Avalonia.Platform;
 
 namespace GooberAlert;
 
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
+public partial class MainWindow : Window {
+    public static BackgroundWorker AudioBG;
+
+    public MainWindow() {
         InitializeComponent();
         //Replace_with_audio
+
+        // Background Worker
+        AudioBG = new BackgroundWorker();
+        AudioBG.WorkerSupportsCancellation = true;
+        AudioBG.DoWork += AudioBG_DoWork;
     }
 
-    private static void Button_OnClick(object? sender, RoutedEventArgs e)
-    {
+    private static void Button_OnClick(object? sender, RoutedEventArgs e) {
         Environment.Exit(0);
     }
 
-    public static void PlayAudio() {
+
+    // A Thread is required or otherwise the Audio object will get disposed causing playback to abruptly stop??
+    // Also thanks to my cute bf for finding this out :3
+    private void AudioBG_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e) {
         // Stream Audio from Avalonia into SFML
         var audio = new SFML.Audio.Music(AssetLoader.Open(new Uri("avares://Peter Alert/Assets/audio.mp3")));
+        
+        while (true) {
+            if (AudioBG.CancellationPending) {
+                audio.Stop();
+                audio.Dispose();
+                e.Cancel = true;
+                return;
+            }
 
-        // A Thread is required or otherwise the Audio object will get disposed causing playback to abruptly stop??
-        // Also thanks to my cute bf for finding this out :3
-        new Thread(() => {
-            while (true) {
-                if (audio.Status != SFML.Audio.SoundStatus.Playing) audio.Play();
+            if (audio.Status != SFML.Audio.SoundStatus.Playing) audio.Play();
                 Thread.Sleep(100);
             }
-        }).Start();
+    }
 
-        // Play the Audio
-        audio.Play();
+    public static void PlayAudio() {
+        // Start Audio Thread
+        AudioBG.RunWorkerAsync();
+    }
+
+    private static void Window_OnClosing(object? sender, WindowClosingEventArgs e) {
+       if (AudioBG.IsBusy) AudioBG.CancelAsync();
+       Environment.Exit(0);
     }
 }
