7d098ec53daf4665af811a401ef07116c49bd12c
Peter Strick <anitastriclk@gmail.com>
2025-04-14 13:29:05 +0200

    Works somewhat
    Windows stops Audio Playback after a while
    
    Linux needs the csfml system package, unable to use .Loop();

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Platform;
 
 namespace GooberAlert;
 
@@ -9,10 +10,19 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        //Replace_with_audio
+        PlayAudio();
     }
 
-    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    private static void Button_OnClick(object? sender, RoutedEventArgs e)
     {
         Environment.Exit(0);
+    }
+
+    public static void PlayAudio() {
+        var audio = new SFML.Audio.Music(AssetLoader.Open(new Uri("avares://Peter Alert/Assets/audio.mp3")));
+        audio.Loop = true;
+        audio.Play();
     }
 }
