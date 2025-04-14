84d9e73561a0f4138ca20ca5c5eba815bbddc722
Peter Strick <anitastriclk@gmail.com>
2025-04-14 13:39:38 +0200

    VLC Test

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Platform;
+using LibVLCSharp.Shared;
 
 namespace GooberAlert;
 
@@ -9,10 +11,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+         //Replace_with_audio
+        PlayAudio();
     }
 
     private void Button_OnClick(object? sender, RoutedEventArgs e)
     {
         Environment.Exit(0);
+    }
+
+    public static void PlayAudio() {
+        // Load libVLC libraries
+        Core.Initialize();
+
+        using var libVLC = new LibVLC(enableDebugLogs: true);
+        using var media = new Media(libVLC, new StreamMediaInput(AssetLoader.Open(new Uri("avares://Peter Alert/Assets/audio.mp3"))));
+        using var mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.Play();
     }
 }
