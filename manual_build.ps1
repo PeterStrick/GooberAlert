e547b9a5a2aab29f1266dfc3fd40aba56e3393fc
Peter Strick <anitastriclk@gmail.com>
2025-04-14 07:32:39 +0200

    Manually building goobers

---
+++ manual_build.ps1
@@ -0,0 +1,101 @@
+﻿# Get Platform to build the Goobers for
+function Show-Menu {
+    Clear
+    Write-Host "Please enter the respective Number of a Platform to build for"
+    Write-Host ""
+    Write-Host "(1) Windows x64"
+    Write-Host "(2) Linux x64"
+    Write-Host "(3) Mac x64"
+    Write-Host ""
+    Write-Host "(4) Windows Arm (64-bit)"
+    Write-Host "(5) Linux Arm (64-bit)"
+    Write-Host "(6) Mac Arm (64-bit)"
+    Write-Host ""
+    $selection = Read-Host "Selected Platform"
+
+    switch ($selection)
+    {
+        '1' {
+            return "win-x64"
+        } '2' {
+            return "linux-x64"
+        } '3' {
+            return "osx-x64"
+        } '4' {
+            return "win-arm64"
+        } '5' {
+            return "linux-arm64"
+        } '6' {
+            return "osx-arm64"
+        } default {
+            Show-Menu
+        }
+    }
+}
+
+# Get Name
+function Get-Name {
+    Clear
+    Write-Host "Enter the Name for the Goober Alert"
+    Write-Host ""
+    return Read-Host "Selected Name"
+}
+
+# Get Button Name
+function Get-ButtonName {
+    Clear
+    Write-Host "Enter the Button Name for the Goober Alert"
+    Write-Host ""
+    return Read-Host "Selected Button Name"
+}
+
+# Get App Icon
+function Get-AppIcon {
+    Clear
+    Write-Host "Enter the Path for the App Icon"
+    Write-Host "This has to be inside the Assets folder. Eg: /Assets/goober.png"
+    Write-Host ""
+    return Read-Host "Selected Asset"
+}
+
+# Get App Icon
+function Get-AppImage {
+    Clear
+    Write-Host "Enter the Path for the App Image"
+    Write-Host "This has to be inside the Assets folder. Eg: /Assets/goober.gif"
+    Write-Host ""
+    return Read-Host "Selected Asset"
+}
+
+$Platform = Show-Menu
+$Name = Get-Name
+$ButtonName = Get-Name
+$AppIcon = Get-AppIcon
+$AppImage = Get-AppImage
+
+# Replace Name
+(Get-Content MainWindow.axaml) | ForEach-Object {
+    $_ -replace 'Peter Alert', $Name
+} | Set-Content MainWindow.axaml -Encoding UTF8
+(Get-Content GooberAlert.csproj) | ForEach-Object {
+    $_ -replace 'Peter Alert', $Name
+} | Set-Content GooberAlert.csproj -Encoding UTF8
+
+# Replace Button Name
+(Get-Content MainWindow.axaml) | ForEach-Object {
+    $_ -replace 'OK', $ButtonName
+} | Set-Content MainWindow.axaml -Encoding UTF8
+
+# Replace App Icon
+(Get-Content MainWindow.axaml) | ForEach-Object {
+    $_ -replace 'Icon="/Assets/Peter.png"', 'Icon="$($AppIcon)"'
+} | Set-Content MainWindow.axaml -Encoding UTF8
+
+# Replace App Image
+(Get-Content MainWindow.axaml) | ForEach-Object {
+    $_ -replace 'Source="/Assets/Peter.png"', 'Source="$($AppImage)"'
+} | Set-Content MainWindow.axaml -Encoding UTF8
+
+dotnet publish GooberAlert.csproj -c Release -r $Platform --sc true -o "./build/$Name"
+
+git reset --hard
